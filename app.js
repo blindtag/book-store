e5725895ab307b47cbe253f51a5e01f2116ee748
blindtag <phylace0@gmail.com>
2021-05-27 16:02:57 -0700

    Passport init

--- app.js
+++ app.js
@@ -4,24 +4,44 @@ const connectDB = require('./config/db')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const path = require('path')
+const passport = require('passport')
+const session = require('express-session')
 
 //Load config
 dotenv.config({path: './config/config.env'})
+
+//passport config
+require('./config/passport')(passport)
+
 //connectDB()
 const app =express()
+
 //Logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
+
 //Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}) )
 app.set('view engine', '.hbs')
 
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
 //Routes
 app.use('/', require('./routes/index'))
-
+app.use('/auth', require('./routes/auth'))
 
 
 
